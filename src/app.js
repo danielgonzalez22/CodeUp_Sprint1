9de7ab8545f5dc8212b1e753b6920d9702277f64
unknown <kavellid63@gmail.com>
2024-05-16 18:28:22 -0300

    database config file, mongodb conection working properly. dotenv variables (port and mongoURI) added

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 const express = require('express')
 const app = express()
+const mongoose = require('./config/database')
+const mainRouter = require('./routes/index')
+const PORT = process.env.PORT || 3000
+app.use(express.json());
+app.use('/', mainRouter)
+app.listen(PORT, () => {
+    console.log (`Server is running on port ${PORT}`)
+})
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
+  }
+)
+  .then(() => console.log('connected to MongoDB successfully'))
+  .catch(error => console.log('MongoDB connection error: ', error))
+
+module.exports = mongoose
