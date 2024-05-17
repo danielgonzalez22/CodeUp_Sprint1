a7feb336c08904be27761df5dd63b9ed3bb2741f
unknown <kavellid63@gmail.com>
2024-05-17 12:35:24 -0300

    dotenv implementation changed from database.js to app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const mongoose = require('./config/database')
 const mainRouter = require('./routes/index')
 const PORT = process.env.PORT || 3000
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use('/', mainRouter)
 app.listen(PORT, () => {
-    console.log (`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 })
