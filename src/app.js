109d665cc853b76854ae67cd8e88083032c070f0
unknown <kavellid63@gmail.com>
2024-05-16 23:58:04 -0300

    added middleware for url-encoded data

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const mongoose = require('./config/database')
 const mainRouter = require('./routes/index')
 const PORT = process.env.PORT || 3000
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/', mainRouter)
 app.listen(PORT, () => {
     console.log (`Server is running on port ${PORT}`)
