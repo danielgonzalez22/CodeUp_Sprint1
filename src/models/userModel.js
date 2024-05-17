55961494b8706d9280cbc34a900754fe54b34c6a
unknown <kavellid63@gmail.com>
2024-05-17 12:36:03 -0300

    added password hashing on user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     lastName: { type: String, required: true },
@@ -16,6 +18,15 @@ const userSchema = new mongoose.Schema({
         enum: ['admin', 'user', 'organizer'],
         default: 'user'
     },
+})
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
 
 module.exports = mongoose.model(
