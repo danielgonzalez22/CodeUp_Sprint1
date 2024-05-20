45d2ba79342c377234c3a4f210f83d26222d5fb4
unknown <kavellid63@gmail.com>
2024-05-20 11:38:30 -0300

    cors (empty rule set) added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 require('dotenv').config()
 const mongoose = require('./config/database')
@@ -6,6 +7,7 @@ const mainRouter = require('./routes/index')
 const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors({}))
 app.use('/', mainRouter)
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
