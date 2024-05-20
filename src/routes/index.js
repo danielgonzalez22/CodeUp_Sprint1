8bb1259d7d928f89b5601a3b9bae92707440b8fe
unknown <kavellid63@gmail.com>
2024-05-20 11:39:59 -0300

    route for events added to main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const userRouter = require('./auth')
+const eventRouter = require('./event')
 
 router.use('/users', userRouter)
+router.use('/events', eventRouter)
 module.exports = router
