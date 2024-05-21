7813ce01153236a33756818d1d2b23d37609cfbb
unknown <kavellid63@gmail.com>
2024-05-20 21:46:07 -0300

    jwt middleware file modified for role-based access control

--- src/services/middleware/authMiddleware.js
+++ src/services/middleware/authMiddleware.js
@@ -1,17 +1,24 @@
 const jwt = require('jsonwebtoken')
 
-const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization')
-    if (!token) {
-        return res.status(401).json({ success: false, message: 'No token, authorization denied.' })
+const authMiddleware = (roles = []) => {
+    return (req, res, next) => {
+        const token = req.header('Authorization')
+        if (!token) {
+            return res.status(401).json({ success: false, message: 'No token, authorization denied.' })
+        }
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            req.user = decoded
+
+            if (roles.length && !roles.includes(req.user.role)) {
+                return res.status(403).json({ success: false, message: 'Forbidden: Access is denied.' })
+            }
+
+            next()
+        } catch (error) {
+            res.status(401).json({ success: false, message: 'Token is not valid.' })
+        }
     }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = decoded
-        next()
-    } catch (error) {
-        res.status(401).json({ success: false, message: 'Token is not valid.' })
-    }  
 }
 
 module.exports = authMiddleware
