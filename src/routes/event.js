42b0e1d2b27f4aff64fec5a59382febea50d140a
unknown <kavellid63@gmail.com>
2024-05-20 21:23:05 -0300

    route for getEventById added

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { createEvent, getAllEvents } = require('../controllers/event/eventController')
+const { createEvent, getAllEvents, getEventById } = require('../controllers/event/eventController')
 
 router.post('/', createEvent)
 router.get('/', getAllEvents)
+router.get('/:id', getEventById)
 
 module.exports = router
