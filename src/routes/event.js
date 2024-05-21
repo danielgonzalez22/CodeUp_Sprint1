c8badc7556dcff7f183c33084a4ecc3e5f4e949e
unknown <kavellid63@gmail.com>
2024-05-20 21:46:42 -0300

    event and place routes modified for using middleware access control

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const authMiddleware = require('../services/middleware/authMiddleware')
 
 const { createEvent, getAllEvents, getEventById } = require('../controllers/event/eventController')
 
+//router.post('/', authMiddleware(['organizer']), createEvent)
 router.post('/', createEvent)
 router.get('/', getAllEvents)
 router.get('/:id', getEventById)
--- src/routes/place.js
+++ src/routes/place.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const authMiddleware = require('../services/middleware/authMiddleware')
+
 
 const { createPlace, getAllPlaces, getPlaceById } = require('../controllers/place/placeController')
 
+//router.post('/', authMiddleware(['admin']), createPlace)
 router.post('/', createPlace)
 router.get('/', getAllPlaces)
 router.get('/:id', getPlaceById)
