6c50a22a1a7bd2f8528ce5e56b5c80e5acd40094
unknown <kavellid63@gmail.com>
2024-05-20 21:18:48 -0300

    routes for getPlaceById added

--- src/routes/place.js
+++ src/routes/place.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { createPlace, getAllPlaces } = require('../controllers/place/placeController')
+const { createPlace, getAllPlaces, getPlaceById } = require('../controllers/place/placeController')
 
 router.post('/', createPlace)
 router.get('/', getAllPlaces)
+router.get('/:id', getPlaceById)
 
 module.exports = router
