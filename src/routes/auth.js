7cb0813971bca1dcc71709697707513329744f65
unknown <kavellid63@gmail.com>
2024-05-22 07:10:15 -0300

    route for updateProfile method added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,13 +2,10 @@ const express = require('express')
 const router = express.Router()
 const authMiddleware = require('../services/middleware/authMiddleware')
 
-const { signUp, logIn } = require('../controllers/user/userController')
+const { signUp, logIn, updateProfile } = require('../controllers/user/userController')
 
 router.post('/signup', signUp)
 router.post('/login', logIn)
-
-router.get('/profile', authMiddleware, (req, res) => {
-    res.json({ success: true, message: 'This is a protected route.', user: req.user })
-})
+router.put('/profile', authMiddleware, updateProfile)
 
 module.exports = router
