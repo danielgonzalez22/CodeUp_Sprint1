d0ba9c1861108c796952153c92188395ca0a0cab
unknown <kavellid63@gmail.com>
2024-05-22 07:30:11 -0300

    route for changepassword method

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 const authMiddleware = require('../services/middleware/authMiddleware')
 
-const { signUp, logIn, updateProfile } = require('../controllers/user/userController')
+const { signUp, logIn, updateProfile, changePassword } = require('../controllers/user/userController')
 
 router.post('/signup', signUp)
 router.post('/login', logIn)
 router.put('/profile', authMiddleware, updateProfile)
+router.put('/profile/passwd', authMiddleware, changePassword)
+
 
 module.exports = router
