c67421fe01d9564e91253ea846fb5671b49e1650
unknown <kavellid63@gmail.com>
2024-05-22 18:18:06 -0300

    joi validation added for 'rating' number when commenting events

--- src/controllers/event/commentValidator.js
+++ src/controllers/event/commentValidator.js
@@ -5,6 +5,12 @@ const objectIdPattern = /^[0-9a-fA-F]{24}$/
 const commentValidator = Joi.object({
     user: Joi.string().pattern(objectIdPattern).required().error(new Error('Invalid user ID.')),
     comment: Joi.string().required().pattern(/^[a-zA-Zñ0-9,. ]+$/).min(1).max(500).error(new Error('Comment must be a string (a-z A-Z ñ 0-9 ,.), between 1 and 500 characters long.')),
+    rating: Joi.number().required().min(0.5).max(10).custom((value) => {
+        if (value % 0.5 !== 0) {
+            return error('Rating must be a number between 0.5 and 10, with 0.5 increments.')
+        }
+        return value
+    }).error(new Error('Rating must be a number between 0.5 and 10, with 0.5 increments.')),
     date: Joi.date().required().error(new Error('Invalid or missing comment date.')),
 })
 
