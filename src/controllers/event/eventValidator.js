23895d3a59e37c5f75a79982436c7a8e55c7887f
unknown <kavellid63@gmail.com>
2024-05-23 00:04:08 -0300

    bad regex fixed on place and event validators

--- src/controllers/event/eventValidator.js
+++ src/controllers/event/eventValidator.js
@@ -5,9 +5,9 @@ const objectIdPattern = /^[0-9a-fA-F]{24}$/
 const eventValidator = Joi.object({
   place: Joi.string().pattern(objectIdPattern).required().error(new Error('Invalid event place ObjectId.')),
   date: Joi.date().required().error(new Error('Invalid or missing event date.')),
-  name: Joi.string().required().pattern(/^[a-zA-Zñ0-9.,- ]+$/).min(3).max(50).error(new Error('Event name must be a string (a-z A-Z ñ 0-9 .,-), between 3 and 50 characters long.')),
+  name: Joi.string().required().pattern(/^[a-zA-Zñ0-9., ]+$/).min(3).max(50).error(new Error('Event name must be a string (a-z A-Z ñ 0-9 .,-), between 3 and 50 characters long.')),
   photo: Joi.string().uri().allow('').optional().error(new Error('Invalid event photo URL.')),
-  description: Joi.string().required().pattern(/^[a-zA-Zñ0-9.,- ]+$/).min(3).max(200).error(new Error('Event description must be a string (a-z A-Z ñ 0-9 .,-), between 3 and 200 characters long.')),
+  description: Joi.string().required().pattern(/^[a-zA-Zñ0-9., ]+$/).min(3).max(200).error(new Error('Event description must be a string (a-z A-Z ñ 0-9 .,-), between 3 and 200 characters long.')),
   attendees: Joi.array().items(Joi.string().pattern(objectIdPattern).error(new Error('Invalid attendee ObjectId.'))).error(new Error('Invalid event attendees array.')),
   minimumAge: Joi.number().integer().min(0).max(40).required().error(new Error('Event minimum age is required and must be a non-negative integer, between 0 and 40.')),
   organizer: Joi.string().pattern(objectIdPattern).required().error(new Error('Invalid event organizer ObjectId.')),
--- src/controllers/place/placeValidator.js
+++ src/controllers/place/placeValidator.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 const objectIdPattern = /^[0-9a-fA-F]{24}$/
 
 const placeValidator = Joi.object({
-    name: Joi.string().required().pattern(/^[a-zA-Zñ0-9.,- ]+$/).min(3).max(30).error(new Error('Place address must be a string with only letters and numbers, between 3 and 30 characters long.')),
+    name: Joi.string().required().pattern(/^[a-zA-Zñ0-9., ]+$/).min(3).max(30).error(new Error('Place address must be a string with only letters and numbers, between 3 and 30 characters long.')),
     address: Joi.string().required().pattern(/^[a-zA-Zñ0-9,. ]+$/).min(3).max(50).error(new Error('Place address must be a string with only letters and numbers, between 3 and 50 characters long.')),
     photo: Joi.string().uri().allow('').optional().error(new Error('Invalid place photo URL.')),
     date: Joi.array().items(Joi.string().pattern(objectIdPattern).error(new Error('Invalid place date ObjectId.'))).optional().error(new Error('Invalid place dates array.')),
